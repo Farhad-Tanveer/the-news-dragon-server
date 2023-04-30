740f4661a917c8701f58d2519deeda9f3a97ce1c
Farhad-Tanveee <farhadtanveer05u@example.com>
2023-04-30 11:43:01 +0200

    added news data

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 3000;
 app.use(cors());
 
 const catagories = require("./data/catagories.json");
+const news = require("./data/news.json");
 
 app.get("/", (req, res) => {
   res.send("Dragon is running");
@@ -13,6 +14,27 @@ app.get("/", (req, res) => {
 
 app.get("/catagories", (req, res) => {
   res.send(catagories);
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
+
+app.get("/categories/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  if (id == 0) {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => n.category_id === id);
+    res.send(categoryNews);
+  }
 });
 
 app.listen(port, () => {
